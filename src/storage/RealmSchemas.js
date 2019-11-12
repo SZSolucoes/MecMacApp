f2361b096f67dc7566059a46fab5c07d3b33b778
roneymaia <roneymaia@gmail.com>
2019-11-12 14:39:06 -0300

    Commit ReamSchemas.js

---
+++ src/storage/RealmSchemas.js
@@ -0,0 +1,122 @@
+/* eslint-disable max-len */
+export const CarSchema = {
+    name: 'Car',
+    properties: {
+        Label: 'string?',
+        Value: 'string?'
+    }
+};
+
+export const MotorbikeSchema = {
+    name: 'Motorbike',
+    properties: {
+        Label: 'string?',
+        Value: 'string?'
+    }
+};
+
+export const TruckSchema = {
+    name: 'Truck',
+    properties: {
+        Label: 'string?',
+        Value: 'string?'
+    }
+};
+
+export const ModelosSchema = {
+    name: 'Modelos',
+    properties: {
+        Label: 'string?',
+        Value: 'int?'
+    }
+};
+
+export const AnosModeloSchema = {
+    name: 'AnosModelo',
+    properties: {
+        Label: 'string?',
+        Value: 'string?'
+    }
+};
+
+export const FipeModeloSchema = {
+    name: 'FipeModelo',
+    properties: {
+        fipeVHCType: 'int?',
+        marcaLabel: 'string?',
+        marcaValue: 'string?',
+        marcaModelos: 'Modelos[]'
+    }
+};
+
+export const FipeAnosModeloSchema = {
+    name: 'FipeAnosModelo',
+    properties: {
+        fipeVHCType: 'int?',
+        marcaLabel: 'string?',
+        marcaValue: 'string?',
+        modeloLabel: 'string?',
+        modeloValue: 'int?',
+        anosModelo: 'AnosModelo[]'
+    }
+};
+
+export const FipeMarcasSchema = {
+    name: 'FipeMarcas',
+    properties: {
+        carros: 'Car[]',
+        motos: 'Motorbike[]',
+        caminhoes: 'Truck[]'
+    }
+};
+
+export const FipeModelosSchema = {
+    name: 'FipeModelos',
+    properties: {
+        fipeModelos: 'FipeModelo[]'
+    }
+};
+
+export const FipeAnosModelosSchema = {
+    name: 'FipeAnosModelos',
+    properties: {
+        fipeAnoModelo: 'FipeAnosModelo[]'
+    }
+};
+
+export const FipeFileVersionSchema = {
+    name: 'FipeFileVersion',
+    properties: {
+        fileVersion: 'int?'
+    }
+};
+
+export const UserVehicleSchema = {
+    name: 'UserVehicle',
+    properties: {
+        user_email: 'string',
+        manufacturer: 'string',
+        model: 'string',
+        year: 'string',
+        price: 'double?',
+        fuel: 'string?',
+        fipe_ref: 'string?',
+        nickname: 'string',
+        quilometers: 'double?'
+    }
+};
+
+export const AllSchemasArray = [
+    CarSchema, 
+    MotorbikeSchema, 
+    TruckSchema, 
+    ModelosSchema, 
+    FipeModeloSchema, 
+    FipeMarcasSchema, 
+    FipeModelosSchema, 
+    FipeFileVersionSchema,
+    AnosModeloSchema,
+    FipeAnosModeloSchema,
+    FipeAnosModelosSchema,
+    UserVehicleSchema
+];
